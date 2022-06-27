981ae409c82faaa3bc7216efde6ff96b668c6b9b
Arifeen Saeed <arifeensaeed@utexas.edu>
2022-06-26 21:09:54 -0500

    Small fixes

--- setup.sh
+++ setup.sh
@@ -1,10 +1,12 @@
 mkdir -p ~/.streamlit/
-
-echo "\
-[server]\n\
-headless = true\n\
-port = $PORT\n\
-enableCORS = false\n\
-\n\
+echo "[theme]
+primaryColor='#ff5353'
+backgroundColor='#f9f4e6'
+secondaryBackgroundColor='#f7e14b'
+textColor='#000000'
+font='serif'
+[server]
+headless = true
+port = $PORT
+enableCORS = false
 " > ~/.streamlit/config.toml
-
